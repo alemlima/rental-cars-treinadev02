756375abfff052131ae4d67744c08fd1d12a356b
Ale <lima.ale@gmail.com>
2019-11-29 16:21:05 -0300

    Added test to verify if no subsidiaries exist

--- spec/features/adm_view_subsidiarys_spec.rb
+++ spec/features/adm_view_subsidiarys_spec.rb
@@ -13,7 +13,7 @@ feature 'Adm view subsidiarys' do
     expect(page).to have_content('12.345.678/9000-00')
     expect(page).to have_content('Rua escorrega lá vai um, 456')
     expect(page).to have_link('Voltar')
-    
+
   end
 
   scenario 'and return to home page' do
@@ -26,5 +26,15 @@ feature 'Adm view subsidiarys' do
     click_on 'Voltar'
 
     expect(current_path).to eq root_path
+  end
+
+  scenario 'if does not have any subsidiary' do
+
+    visit root_path
+    click_on 'Filiais'
+
+    expect(page).to have_content('Não existem filiais cadastradas.')
+    expect(page).to have_link('Voltar')
+
   end
 end
