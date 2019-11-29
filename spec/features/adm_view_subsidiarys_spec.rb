085a7bf3858603a297f779447da809ae01eaf6a8
Ale <lima.ale@gmail.com>
2019-11-29 16:00:04 -0300

    Alterada view show e teste de filiais

--- spec/features/adm_view_subsidiarys_spec.rb
+++ spec/features/adm_view_subsidiarys_spec.rb
@@ -2,20 +2,23 @@ require 'rails_helper'
 
 feature 'Adm view subsidiarys' do
   scenario 'successfully' do
-    Subsidiary.create(name: 'São Paulo')
-    Subsidiary.create(name: 'Campinas')
+    Subsidiary.create!(name: 'São Paulo', cnpj: '12.345.678/9000-00', address: 'Rua escorrega lá vai um, 456')
+    Subsidiary.create!(name: 'Campinas', cnpj: '33.345.332/9654-00', address: 'Rua da bica, 123')
 
     visit root_path
     click_on 'Filiais'
     click_on 'São Paulo'
 
     expect(page).to have_content('São Paulo')
+    expect(page).to have_content('12.345.678/9000-00')
+    expect(page).to have_content('Rua escorrega lá vai um, 456')
     expect(page).to have_link('Voltar')
+    
   end
 
   scenario 'and return to home page' do
-    Subsidiary.create(name: 'São Paulo')
-    Subsidiary.create(name: 'Campinas')
+    Subsidiary.create!(name: 'São Paulo', cnpj: '12.345.678/9000-00', address: 'Rua escorrega lá vai um, 456')
+    Subsidiary.create!(name: 'Campinas', cnpj: '12.345.678/9000-00', address: 'Rua da bica, 123')
 
     visit root_path
     click_on 'Filiais'
