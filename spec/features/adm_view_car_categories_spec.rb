b6163721f207b8ef09d0b45887686a4031c3f36d
Ale <lima.ale@gmail.com>
2019-12-02 10:42:11 -0300

    Changed car_category spec and show view

--- spec/features/adm_view_car_categories_spec.rb
+++ spec/features/adm_view_car_categories_spec.rb
@@ -3,20 +3,23 @@ require 'rails_helper'
 feature 'Adm view car categories' do
   scenario 'successfully' do
     #Arrange
-    CarCategory.create(name: 'Hatches')
-    CarCategory.create(name: 'Sedans')
+    CarCategory.create(name: 'Hatches',daily_rate: 100, car_insurance: 50, third_party_insurance: 60)
+    CarCategory.create(name: 'Sedans',daily_rate: 130, car_insurance: 60, third_party_insurance: 70)
     #Act
     visit root_path
     click_on 'Categoria'
     click_on 'Hatches'
     #Assert
     expect(page).to have_content('Hatches')
+    expect(page).to have_content(100)
+    expect(page).to have_content(50)
+    expect(page).to have_content(60)
     expect(page).to have_link('Voltar')
   end
 
   scenario 'and return to home page' do
-    CarCategory.create(name: 'Hatches')
-    CarCategory.create(name: 'Sedans')
+    CarCategory.create(name: 'Hatches',daily_rate: 100, car_insurance: 50, third_party_insurance: 60)
+    CarCategory.create(name: 'Sedans',daily_rate: 130, car_insurance: 60, third_party_insurance: 70)
 
     visit root_path
     click_on 'Categoria'
