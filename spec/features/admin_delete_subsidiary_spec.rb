83e0647d56c09dcf67981cead5ce9450fc0e6eeb
Ale <lima.ale@gmail.com>
2019-12-04 21:51:26 -0300

    Created delete subsidiary functionality for admin

--- spec/features/admin_delete_subsidiary_spec.rb
+++ spec/features/admin_delete_subsidiary_spec.rb
@@ -1,16 +1,16 @@
 require 'rails_helper'
 
 feature 'Admin delete subsidiary' do
-  xscenario 'successfully' do
+  scenario 'successfully' do
     Subsidiary.create!(name: 'São Paulo', cnpj: '12.345.678/9000-00', address: 'Rua escorrega lá vai um, 456')
     Subsidiary.create!(name: 'Campinas', cnpj: '12.345.678/3400-00', address: 'Rua dois, 56')
 
     visit root_path
     click_on 'Filiais'
-    #click_on 'São Paulo'
+    click_on 'São Paulo'
     click_on 'Apagar filial'
 
     expect(page).to have_content('Filial excluída com sucesso.')
-    expect(pag).to not_have('São Paulo')
+    expect(page).not_to have_content('São Paulo')
   end  
 end
